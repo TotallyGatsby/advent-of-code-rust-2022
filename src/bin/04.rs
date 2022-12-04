290052fe1157a0802e30ffa18cab4361d8f245a3
TotallyGatsby <700673+TotallyGatsby@users.noreply.github.com>
2022-12-04 11:27:25 -0800

    Solve day 4, add a helper to split a string into integers

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,9 +1,50 @@
+use advent_of_code::helpers::split_to_ints;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    input.lines().try_fold(0, |score, line| {
+        // Example input is 3-5,4-9
+        // So we split out some vars, first by the comma to get (3-5, 4-9)
+        let (elf1, elf2) = line.split_once(",")?;
+        let (elf1_min, elf1_max) = split_to_ints(elf1, "-")?;
+        let (elf2_min, elf2_max) = split_to_ints(elf2, "-")?;
+
+        // Then into individual values (3,5), (4,9)
+
+        // If both elves share one boundary, one must be the subset of the other
+        if elf1_min == elf2_min || elf1_max == elf2_max {
+            return Some(score + 1);
+        }
+
+        // Store which elf holds the min, and which holds the max
+        let min_elf = if elf1_min <= elf2_min { 1 } else { 2 };
+        let max_elf = if elf1_max >= elf2_max { 1 } else { 2 };
+
+        // If one elf has the min and the max, it has a range that encompases the other elf
+        if min_elf == max_elf {
+            return Some(score + 1);
+        }
+
+        // If any input is invalid or if there's not a match, do not increment the accumulator
+        Some(score)
+    })
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    input.lines().try_fold(0, |score, line| {
+        // Example input is 3-5,4-9
+        // So we split out some vars, first by the comma to get (3-5, 4-9)
+        let (elf1, elf2) = line.split_once(",")?;
+        let (elf1_min, elf1_max) = split_to_ints(elf1, "-")?;
+        let (elf2_min, elf2_max) = split_to_ints(elf2, "-")?;
+
+        if elf1_min == elf2_min
+            || elf1_max == elf2_max
+            || (elf1_max >= elf2_min && elf1_min <= elf2_max)
+        {
+            return Some(score + 1);
+        }
+        Some(score)
+    })
 }
 
 fn main() {
@@ -19,12 +60,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(4));
     }
 
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(6));
     }
 }
--- src/helpers.rs
+++ src/helpers.rs
@@ -1,4 +1,43 @@
+use std::str::FromStr;
+
 /*
  * Use this file if you want to extract helpers from your solutions.
  * Example import from this file: `use advent_of_code::helpers::example_fn;`.
  */
+
+pub fn split_to_ints(input: &str, delim: &str) -> Option<(u32, u32)> {
+    let (first_str, second_str) = input.trim().split_once(delim)?;
+
+    let first = u32::from_str(first_str).ok()?;
+    let second = u32::from_str(second_str).ok()?;
+
+    Some((first, second))
+}
+
+#[test]
+fn test_parse_input() {
+    // Test valid input
+    let input = "4-5";
+    let result = split_to_ints(input, "-");
+    assert_eq!(result, Some((4, 5)));
+
+    // Test input with leading and trailing whitespace
+    let input = " 4-5 ";
+    let result = split_to_ints(input, "-");
+    assert_eq!(result, Some((4, 5)));
+
+    // Test input with only one number
+    let input = "4-";
+    let result = split_to_ints(input, "-");
+    assert_eq!(result, None);
+
+    // Test input with non-numeric characters
+    let input = "4-5a";
+    let result = split_to_ints(input, "-");
+    assert_eq!(result, None);
+
+    // Test input with negative numbers
+    let input = "-4--5";
+    let result = split_to_ints(input, "-");
+    assert_eq!(result, None);
+}
